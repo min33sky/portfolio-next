3d65ad65fe219d087ae2dbeaa0a1a37e2ae80797
min33sky <min33sky@naver.com>
2022-10-02 21:10:17 +0900

    feat: hero finish

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import React from 'react';
 import { Cursor, useTypewriter } from 'react-simple-typewriter';
 import BackgroundCircle from './BackgroundCircle';
@@ -16,10 +17,35 @@ export default function Hero() {
   return (
     <div className="flex h-screen flex-col items-center justify-center space-y-8 overflow-hidden text-center">
       <BackgroundCircle />
-      <h1>
-        <span>{text}</span>
-        <Cursor cursorColor="#F7AB0A" />
-      </h1>
+      <img
+        className="relative mx-auto h-32 w-32 rounded-full object-contain"
+        src="https://openimage.interpark.com/goods_image_big/4/5/6/5/8410224565_l.jpg"
+        alt=""
+      />
+      <div className="z-20">
+        <h2 className="pb-2 text-sm uppercase tracking-[15px] text-gray-500">
+          Software Engineer
+        </h2>
+        <h1 className="px-10 text-5xl font-semibold lg:text-6xl">
+          <span className="mr-3">{text}</span>
+          <Cursor cursorColor="#F7AB0A" />
+        </h1>
+
+        <div className="pt-5">
+          <Link href="#about">
+            <button className="heroButton">About</button>
+          </Link>
+          <Link href="#experience">
+            <button className="heroButton">Experience</button>
+          </Link>
+          <Link href="#skills">
+            <button className="heroButton">Skills</button>
+          </Link>
+          <Link href="#projects">
+            <button className="heroButton">Projects</button>
+          </Link>
+        </div>
+      </div>
     </div>
   );
 }
